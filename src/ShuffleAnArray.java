527853fdd328f51e33e56a2763a93c88b043293e
BigTiannn <leetian.pp@gmail.com>
2017-07-10 00:20:31 -0700

    384. Shuffle an Array

--- src/ShuffleAnArray.java
+++ src/ShuffleAnArray.java
@@ -15,9 +15,29 @@ import java.util.Random;
  * // Returns the random shuffling of array [1,2,3].
  * solution.shuffle();
  * 
+ * Proof:
+ * The probability that ith element (including the last one) goes to last position is 1/n, 
+ * because we randomly pick an element in first iteration.
+ *
+ * The probability that ith element goes to second last position can be proved to be 1/n 
+ * dividing it in two cases.
+ * Case 1: i = n-1 (index of last element):
+ * The probability of last element going to second last position is = (probability that 
+ * last element doesn't stay at its original position) x (probability that the index 
+ * picked in previous step is picked again so that the last element is swapped)
+ * So the probability = ((n-1)/n) x (1/(n-1)) = 1/n
+ * Case 2: 0 < i < n-1 (index of non-last):
+ * The probability of ith element going to second last position = (probability that ith 
+ * element is not picked in previous iteration) x (probability that ith element is picked 
+ * in this iteration)
+ * So the probability = ((n-1)/n) x (1/(n-1)) = 1/n
+ * 
+ * We can easily generalize above proof for any other position.
+ * 
  * @author BigTiannn
  *
  */
+
 public class ShuffleAnArray {
   private int[] initialNums;
   private Random random;
