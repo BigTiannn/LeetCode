eab20066d7d978e4be6daa29e2e445151782031f
BigTiannn <leetian.pp@gmail.com>
2017-07-16 17:42:35 -0700

    240. Search a 2D Matrix II 74. Search a 2D Matrix

--- src/SearchA2DMatrix.java
+++ src/SearchA2DMatrix.java
@@ -4,6 +4,14 @@
  * This matrix has the following properties:
  * Integers in each row are sorted from left to right.
  * The first integer of each row is greater than the last integer of the previous row.
+ * For example,
+ * Consider the following matrix:
+ * [
+ *  [1,   3,  5,  7],
+ *  [10, 11, 16, 20],
+ *  [23, 30, 34, 50]
+ * ]
+ * Given target = 3, return true.
  * 
  * @author BigTiannn {11-12-2014}
  *
@@ -39,27 +47,63 @@ public class SearchA2DMatrix {
         return false;
     }
 	
-	// binary search
+	// solution 2: binary search
 	public boolean searchMatrix_2(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+    int m = matrix.length;
+    int n = matrix[0].length;
         
-        int low = 0, high = m * n - 1;
+    int low = 0, high = m * n - 1;
         
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int val = matrix[mid / n][mid % n];
-            if (val > target) {
-                high = mid - 1;
-            } else if (val < target) {
-                low = mid + 1;
-            } else {
-                return true;
-            }
-        }
-        
-        return false;
-    }
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      int val = matrix[mid / n][mid % n];
+      if (val > target) {
+        high = mid - 1;
+      } else if (val < target) {
+        low = mid + 1;
+      } else {
+        return true;
+      }
+    }    
+    return false;
+  }
+	
+	// solution 3: improved the solution 2, two-time binary search
+	public boolean solution(int[][] matrix, int target) {
+	  if (matrix == null || matrix.length == 0 || matrix[0].length == 0) 
+	    return false;
+	  
+	  int m = matrix.length, n = matrix[0].length;
+	  int low = 0, high = m - 1;
+	  while (low <= high) {
+	    int mid = (low + high) / 2;
+	    if (matrix[mid][0] == target) {
+	      return true;
+	    } else if (matrix[mid][0] > target) {
+	      high = mid - 1;
+	    } else {
+	      low = mid + 1;
+	    }
+	  }
+	  
+	  if (high < 0) return false;
+	  
+	  int row = high;
+	  low = 0;
+	  high = n - 1;
+	  while (low <= high) {
+	    int mid = (low + high) / 2;
+	    if (matrix[row][mid] == target) {
+	      return true;
+	    } else if (matrix[row][mid] > target) {
+	      high = mid - 1;
+	    } else {
+	      low = mid + 1;
+	    }
+	  }
+	  
+	  return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
