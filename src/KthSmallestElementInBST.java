4e8e2f3f17bd1acd7a46c1127938fbb6cb2fe4af
BigTiannn <leetian.pp@gmail.com>
2017-07-18 00:10:20 -0700

    287. Find the Duplicate Number

---
+++ src/KthSmallestElementInBST.java
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/#/description
+ * Given a binary search tree, write a function kthSmallest to find the kth 
+ * smallest element in it.
+ * Note: 
+ * You may assume k is always valid, 1 <= k <= BST's total elements.
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to 
+ * find the kth smallest frequently? How would you optimize the kthSmallest routine?
+ * 
+ * @author BigTiannn
+ *
+ */
+
+public class KthSmallestElementInBST {
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode (int x) { 
+      val = x;
+    }
+  }
+  
+  public int kthSmallest(TreeNode root, int k) {
+    return 0;
+  }
+  
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
