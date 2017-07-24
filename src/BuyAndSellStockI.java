6e33501284664d44397bdf1d2fc35cdad58cb8db
BigTiannn <leetian.pp@gmail.com>
2017-07-23 17:00:39 -0700

    121. Best Time to Buy and Sell Stock

--- src/BuyAndSellStockI.java
+++ src/BuyAndSellStockI.java
@@ -3,30 +3,34 @@
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * At most one transaction (ie, buy one and sell one share of the stock), 
  * design an algorithm to find the maximum profit.
+ * Example 1:
+ * Input: [7, 1, 5, 3, 6, 4]
+ * Output: 5
+ * max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+ * 
+ * Example 2:
+ * Input: [7, 6, 4, 3, 1]
+ * Output: 0
+ * In this case, no transaction is done, i.e. max profit = 0.
+ *
  * @author BigTiannn
  *
  */
+
 public class BuyAndSellStockI {
 	
 	public static int maxProfit(int[] prices) {
-		if (prices == null || prices.length < 2)
-			return 0;
-		
-        int max = prices[0], min = prices[0], max_prof = 0;
-        for (int i = 1; i < prices.length; i ++) {
-        	if (prices[i] > max) {
-        		max = prices[i];
-        		if (max - min > max_prof)
-        			max_prof = max - min;
-        	}
-        	if (prices[i] < min) {
-        		min = prices[i];
-        		max = -1;
-        	}
-        }
-        
-        return max_prof;
+		if (prices == null || prices.length < 2)  return 0;
+    
+    int maxProfit = prices[1] - prices[0], minPrice = prices[0];
+    for (int i = 1; i < prices.length; i ++) {
+      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+      minPrice = Math.min(minPrice, prices[i]);
     }
+    if (maxProfit < 0) maxProfit = 0;
+    
+    return maxProfit;
+  }
 	
 	public static void main (String[] args) {
 		int[] prices = {4, 2, 1};
