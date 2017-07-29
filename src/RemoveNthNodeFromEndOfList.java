21218fd2a32a6f281219b6be8b907c2c7ed3dbba
BigTiannn <leetian.pp@gmail.com>
2017-07-29 00:34:09 -0700

    19. Remove Nth Node From End of List

--- src/RemoveNthNodeFromEndOfList.java
+++ src/RemoveNthNodeFromEndOfList.java
@@ -1,5 +1,5 @@
 /**
- * https://oj.leetcode.com/problems/remove-nth-node-from-end-of-list/
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/tabs/description
  * Given a linked list, remove the nth node from the end of list and return its head.
  * For example,
  * Given linked list: 1->2->3->4->5, and n = 2.
@@ -24,44 +24,44 @@ public class RemoveNthNodeFromEndOfList {
 	}
 	
 	public static ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode endPtr = head, nthPtr = head, nthPrev = head;
-        
-        // move endPtr to be n nodes ahead of nthPtr
-        int i = 0;
-        for (; endPtr != null && i < n; i ++)
-        	endPtr = endPtr.next;
-        
-        // check the validation of n
-        if (i != n) {
-        	System.out.println("Invalid argument n: n should be less or equal to the length of the list.");
-        	return head;
-        }
-        
-        // move nthPtr to the n_th node from the end of list
-        for (; endPtr != null; endPtr = endPtr.next, nthPtr = nthPtr.next)
-        	nthPrev = nthPtr;
-        
-        if (nthPtr == head)
-        	return head.next;
-        
-        nthPrev.next = nthPtr.next;
-        return head;
+    ListNode endPtr = head, nthPtr = head, nthPrev = head;
+    
+    // move endPtr to be n nodes ahead of nthPtr
+    int i = 0;
+    for (; endPtr != null && i < n; i ++)
+    	endPtr = endPtr.next;
+    
+    // check the validation of n
+    if (i != n) {
+    	System.out.println("Invalid argument n: n should be less or equal to the length of the list.");
+    	return head;
     }
+    
+    // move nthPtr to the n_th node from the end of list
+    for (; endPtr != null; endPtr = endPtr.next, nthPtr = nthPtr.next)
+    	nthPrev = nthPtr;
+    
+    if (nthPtr == head)
+    	return head.next;
+    
+    nthPrev.next = nthPtr.next;
+    return head;
+  }
 	
 	public ListNode removeNthFromEnd_2(ListNode head, int n) {
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode ptr = dummy, nthNode = dummy;
-        for (int i = 0; i < n; i ++)
-            ptr = ptr.next;
-            
-        while (ptr.next != null) {
-            ptr = ptr.next;
-            nthNode = nthNode.next;
-        }
-        nthNode.next = nthNode.next.next;
-        return dummy.next;
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+    ListNode ptr = dummy, nthNode = dummy;
+    for (int i = 0; i < n; i ++)
+      ptr = ptr.next;
+        
+    while (ptr.next != null) {
+      ptr = ptr.next;
+      nthNode = nthNode.next;
     }
+    nthNode.next = nthNode.next.next;
+    return dummy.next;
+  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
