394c999acfdc9e3258f139e4c7ffd9dd34e6810a
BigTiannn <leetian.pp@gmail.com>
2017-07-29 18:50:09 -0700

    138. Copy List with Random Pointer

--- src/CopyRandomList.java
+++ src/CopyRandomList.java
@@ -27,64 +27,78 @@ public class CopyRandomList {
 		}
 	};
 	
-	public RandomListNode copyRandomList(RandomListNode head) {
-        Map<RandomListNode, RandomListNode> map = new HashMap<>();
-        RandomListNode dummy = new RandomListNode(-1);
-        RandomListNode p = head, prev = dummy;
-        while (p != null) {
-            RandomListNode node = new RandomListNode(p.label);
-            prev.next = node;
-            map.put(p, node);
-            prev = prev.next;
-            p = p.next;
-        }
-        
-        p = head;
-        RandomListNode q = dummy.next;
-        while (p != null) {
-            if (p.random != null) {
-                q.random = map.get(p.random);
-            }
-            p = p.next;
-            q = q.next;
-        }
-        
-        return dummy.next;
+	public RandomListNode solution(RandomListNode head) {
+    Map<RandomListNode, RandomListNode> map = new HashMap<>();
+    RandomListNode dummy = new RandomListNode(-1);
+    RandomListNode p = head, prev = dummy;
+    while (p != null) {
+      RandomListNode node = new RandomListNode(p.label);
+      prev.next = node;
+      map.put(p, node);
+      prev = prev.next;
+      p = p.next;
     }
+        
+    p = head;
+    RandomListNode q = dummy.next;
+    while (p != null) {
+      if (p.random != null) {
+          q.random = map.get(p.random);
+      }
+      p = p.next;
+      q = q.next;
+    }
+    
+    return dummy.next;
+  }
 	
-	public RandomListNode copyRandomList_2(RandomListNode head) {
-        if (head == null)   return null;
-        // copy each node and append the newly created node to the original one
-        RandomListNode p = head;
-        while (p != null) {
-            RandomListNode node = new RandomListNode(p.label);
-            node.next = p.next;
-            p.next = node;
-            p = node.next;
-        }
-        
-        // set the value of random field
-        p = head;
-        while (p != null) {
-            if (p.random != null)
-                p.next.random = p.random.next;
-            p = p.next.next;
-        }
-        
-        // return the newly created list
-        p = head;
-        RandomListNode dummy = new RandomListNode(-1);
-        RandomListNode q = dummy;
-        while (p != null) {
-            q.next = p.next;
-            q = q.next;
-            p.next = q.next;
-            p = p.next;
-        }
-        
-        return dummy.next;
+	public RandomListNode solution2(RandomListNode head) {
+    if (head == null)   return null;
+    // copy each node and append the newly created node to the original one
+    RandomListNode p = head;
+    while (p != null) {
+      RandomListNode node = new RandomListNode(p.label);
+      node.next = p.next;
+      p.next = node;
+      p = node.next;
     }
+        
+    // set the value of random field
+    p = head;
+    while (p != null) {
+      if (p.random != null) p.next.random = p.random.next;
+      p = p.next.next;
+    }
+        
+    // return the newly created list
+    p = head;
+    RandomListNode dummy = new RandomListNode(-1);
+    RandomListNode q = dummy;
+    while (p != null) {
+      q.next = p.next;
+      q = q.next;
+      p.next = q.next;
+      p = p.next;
+   }
+    
+    return dummy.next;
+  }
 
+	public RandomListNode solution3(RandomListNode head) {
+	  // the map is "original node" - "new node" pair
+    HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
+    for (RandomListNode node = head; node != null; node = node.next) {
+      map.put(node, new RandomListNode(node.label));
+    }
+    for (Map.Entry<RandomListNode, RandomListNode> entry : map.entrySet()) {
+      RandomListNode newNode = (RandomListNode)entry.getValue();
+      RandomListNode oriNode = (RandomListNode)entry.getKey();
+      newNode.next = map.get(oriNode.next);
+      newNode.random = map.get(oriNode.random);
+    }
+    return map.get(head);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
