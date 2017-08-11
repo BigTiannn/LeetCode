60cb58fbc5533cb375a5e8bc23631b6a35c209a7
BigTiannn <leetian.pp@gmail.com>
2017-08-10 23:49:42 -0700

    34. Search for a range

--- src/SearchForARange.java
+++ src/SearchForARange.java
@@ -12,34 +12,63 @@
  */
 
 public class SearchForARange {
-	
-    public int[] searchRange(int[] A, int target) {
-    	int low = 0, high = A.length - 1;
-        int ret[] = {-1, -1};
-        
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (target < A[mid]) {
-                high = mid - 1;
-            } else if  (target > A[mid]){
-                low = mid + 1;
-            } else {
-                while (mid >= 0 && A[mid] == target)
-                    mid --;
-                ret[0] = ++ mid;
-                while (mid < A.length && A[mid] == target)
-                    mid ++;
-                ret[1] = -- mid;
-                break;
-            }
-        }
-        
-        return ret;
+  public int[] solution(int[] A, int target) {
+  	int low = 0, high = A.length - 1;
+    int ret[] = {-1, -1};
+      
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (target < A[mid]) {
+        high = mid - 1;
+      } else if  (target > A[mid]){
+        low = mid + 1;
+      } else {
+        while (mid >= 0 && A[mid] == target)
+          mid --;
+        ret[0] = ++ mid;
+        while (mid < A.length && A[mid] == target)
+          mid ++;
+        ret[1] = -- mid;
+        break;
+      }
     }
-
+    return ret;
+  }
+  
+  // two binary searches for the beginning and end of the range,
+  // If A[mid] < target, then the range must begins on the right of mid (i = mid + 1)
+  // If A[mid] > target, it means the range must begins on the left of mid (j = mid - 1)
+  // If A[mid] = target, then the range must begins on the left of or at mid (j= mid)
+  // Since we would move the search range to the same side for case 2 and 3: If A[mid] >= target, j = mid;
+  public int[] solution2(int[] nums, int target) {
+    int[] res = {-1, -1};
+    int low = 0, high = nums.length - 1;
+    if (nums.length == 0) return res;
+    // search for left boundary
+    while (low < high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] < target) low = mid + 1;
+        else high = mid;
+    }
+    if (nums[low] != target)  return res;
+    res[0] = high;
+    // search for right boundary
+    high = nums.length - 1;
+    while (low < high) {
+        int mid = (low + high) / 2 + 1;
+        if (nums[mid] > target) high = mid - 1;
+        else low = mid;
+    }
+    res[1] = low;
+    return res;
+  }
+  
+  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	  SearchForARange test = new SearchForARange();
+	  int[] nums = {};
+	  test.solution2(nums, 0);
 	}
 
 }
