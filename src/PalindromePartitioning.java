ed8631abece325703b03031d2755af0e3d627f07
BigTiannn <leetian.pp@gmail.com>
2017-08-14 00:43:52 -0700

    131. Palindrome Partitioning

--- src/PalindromePartitioning.java
+++ src/PalindromePartitioning.java
@@ -19,36 +19,33 @@ public class PalindromePartitioning {
 	
 	// time complexity O(2^(n - 1))
 	public List<List<String>> partition(String s) {
-        List<List<String>> reslist = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        
-        dfs(s, 0, reslist, list);
-        
-        return reslist;
+    List<List<String>> reslist = new ArrayList<>();
+    List<String> list = new ArrayList<>();
+    dfs(s, 0, reslist, list);
+    return reslist;
+  }
+    
+  public void dfs(String s, int start, List<List<String>> reslist, List<String> list) {
+    if (start == s.length()) {
+      reslist.add(new ArrayList<String>(list));
+      return;
     }
     
-    public void dfs(String s, int start, List<List<String>> reslist, List<String> list) {
-        if (start == s.length()) {
-            reslist.add(new ArrayList<String>(list));
-            return;
-        }
-        
-        for (int i = start; i < s.length(); i ++) {
-            if (isPalindrome(s, start, i)) {
-                list.add(s.substring(start, i + 1));
-                dfs(s, i + 1, reslist, list);
-                list.remove(list.size() - 1);
-            }
-        }
+    for (int i = start; i < s.length(); i ++) {
+      if (isPalindrome(s, start, i)) {
+        list.add(s.substring(start, i + 1));
+        dfs(s, i + 1, reslist, list);
+        list.remove(list.size() - 1);
+      }
     }
+  }
     
-    public boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start ++) != s.charAt(end --))
-                return false;
-        }
-        return true;
+  public boolean isPalindrome(String s, int start, int end) {
+    while (start < end) {
+      if (s.charAt(start ++) != s.charAt(end --)) return false;
     }
+    return true;
+  }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
