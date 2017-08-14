b1c0def48d9ac3bd7c92da20b6b2a0348f3e4099
BigTiannn <leetian.pp@gmail.com>
2017-08-13 22:35:44 -0700

    150. Evaluate Reverse Polish Notation

--- src/EvalRPN.java
+++ src/EvalRPN.java
@@ -10,41 +10,74 @@ import java.util.Stack;
 
 public class EvalRPN {
 	// no sanity check (suppose the input string is valid RPT)
-	public static int Solution (String[] tokens) {
-		if (tokens == null || tokens.length == 0)
-			return 0;
+	public static int solution (String[] tokens) {
+		if (tokens == null || tokens.length == 0) return 0;
 		
 		Stack<Integer> stack = new Stack<>();
 		for (String t : tokens) {
-		    if (!isOperator(t)) {
-		        stack.add(Integer.parseInt(t));
-		    } else {
-		        int num2 = stack.pop();
-		        int num1 = stack.pop();
-		        switch(t.charAt(0)) {
-		        case '+':   stack.add(num1 + num2);	break;
-		        case '-':   stack.add(num1 - num2);	break;
-		        case '*':   stack.add(num1 * num2);	break;
-		        case '/':   stack.add(num1 / num2);	break;
-		        }
-		    }
+	    if (!isOperator(t)) {
+        stack.add(Integer.parseInt(t));
+	    } else {
+        int num2 = stack.pop();
+        int num1 = stack.pop();
+        switch(t.charAt(0)) {
+	        case '+':   stack.add(num1 + num2);	break;
+	        case '-':   stack.add(num1 - num2);	break;
+	        case '*':   stack.add(num1 * num2);	break;
+	        case '/':   stack.add(num1 / num2);	break;
+        }
+	    }
 		}
-		
 		return stack.pop();
 	}
 	
 	private static boolean isOperator(String s) {
-        if (s.length() == 1) {
-            if (s.charAt(0) == '+' || s.charAt(0) == '-' || s.charAt(0) == '*' || s.charAt(0) == '/')
-                return true;
-        }
-        return false;
+    if (s.length() == 1) {
+      if (s.charAt(0) == '+' || s.charAt(0) == '-' || s.charAt(0) == '*' || s.charAt(0) == '/')
+        return true;
     }
+    return false;
+  }
+	
+	public static int solution2(String[] tokens) {
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < tokens.length; i ++) {
+      int num1 = 0, num2 = 0;
+      switch (tokens[i]) {
+        case "+":
+          num2 = stack.pop();
+          num1 = stack.pop();
+          stack.add(num1 + num2);
+          break;
+        case "-":
+          num2 = stack.pop();
+          num1 = stack.pop();
+          stack.add(num1 - num2);
+          break;
+        case "*":
+          num2 = stack.pop();
+          num1 = stack.pop();
+          stack.add(num1 * num2);
+          break;
+        case "/":
+          num2 = stack.pop();
+          num1 = stack.pop();
+          stack.add(num1 / num2);
+          break;
+        default:
+          stack.add(Integer.parseInt(tokens[i]));
+          break;
+      }
+    }
+    return stack.pop();
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		String[] s = {"-78","-33","196","+","-19","-","115","+","-","-99","/","-18","8","*","-86","-","-","16","/","26","-14","-","-","47","-","101","-","163","*","143","-","0","-","171","+","120","*","-60","+","156","/","173","/","-24","11","+","21","/","*","44","*","180","70","-40","-","*","86","132","-84","+","*","-","38","/","/","21","28","/","+","83","/","-31","156","-","+","28","/","95","-","120","+","8","*","90","-","-94","*","-73","/","-62","/","93","*","196","-","-59","+","187","-","143","/","-79","-89","+","-"};
 		String[] s = {"4","-2","/","2","-3","-","-"};
-		System.out.println(Solution(s));
+		String[] s1 = {"3", "-4", "+"};
+		System.out.println(solution2(s1));
+		System.out.println(Integer.parseInt("-3"));
 	}
 }
