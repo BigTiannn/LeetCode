f44000466223805e391750488d68d388c8814d98
BigTiannn <leetian.pp@gmail.com>
2017-08-18 00:50:48 -0700

    130. Surrounded Regions;

--- src/SurroundedRegions.java
+++ src/SurroundedRegions.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 
 public class SurroundedRegions {
@@ -17,7 +19,7 @@ public class SurroundedRegions {
 	
 	// BFS to find those inactive 'O's, 
 	// then the remaining 'O's are those need to be painted to 'X'
-	public static void Solution (char[][] board) {
+	public static void solution (char[][] board) {
 		if (board == null || board.length == 0)
 			return ;
 		
@@ -27,22 +29,14 @@ public class SurroundedRegions {
 		
 		// top & bottom border
 		for (int i = 0; i < n; i ++) {
-			if (board[0][i] == 'O') {
-				list.add(new Coordinate(0, i));
-			}
-			if (board[m-1][i] == 'O') {
-				list.add(new Coordinate(m - 1, i));
-			}
+			if (board[0][i] == 'O')  list.add(new Coordinate(0, i));
+			if (board[m - 1][i] == 'O')  list.add(new Coordinate(m - 1, i));
 		}
 		
 		// left & right border
 		for (int i = 0; i < m; i ++) {
-			if (board[i][0] == 'O') {
-				list.add(new Coordinate(i, 0));
-			}
-			if (board[i][n-1] == 'O') {
-				list.add(new Coordinate(i, n - 1 ));
-			}
+			if (board[i][0] == 'O')  list.add(new Coordinate(i, 0));
+			if (board[i][n - 1] == 'O')  list.add(new Coordinate(i, n - 1 ));
 		}
 		
 		// BFS to find all inactive 'O' and paint to 'P'
@@ -63,15 +57,77 @@ public class SurroundedRegions {
 		
 		for (int i = 0; i < m; i ++) {
 			for (int j = 0; j < n; j ++) {
-				if (board[i][j] == 'O')
-					board[i][j] = 'X';
-				if (board[i][j] == 'P')
-					board[i][j] = 'O';
+				if (board[i][j] == 'O') board[i][j] = 'X';
+				if (board[i][j] == 'P') board[i][j] = 'O';
 			}
 		}
 		
 		return;
 	}
+	
+	// bfs
+	public static void solution2(char[][] board) {
+    if (board == null || board.length == 0 || board[0].length == 0)    return;
+    
+    int m = board.length, n = board[0].length;
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 0; i < n; i ++) {
+      if (board[0][i] == 'O') queue.add(i);
+      if (board[m - 1][i] == 'O') queue.add((m - 1) * n + i);
+    }
+    for (int i = 0; i < m; i ++) {
+      if (board[i][0] == 'O') queue.add(i * n);
+      if (board[i][n - 1] == 'O') queue.add(i * n + n - 1);
+    }
+    while (!queue.isEmpty()) {
+      int pos = queue.poll();
+      int row = pos / n, col = pos % n;
+      board[row][col] = '1';
+      if (row - 1 >= 0 && board[row - 1][col] == 'O') queue.add((row - 1) * n + col);
+      if (row + 1 < m && board[row + 1][col] == 'O') queue.add((row + 1) * n + col);
+      if (col - 1 >= 0 && board[row][col - 1] == 'O') queue.add(row * n + col - 1);
+      if (col + 1 < n && board[row][col + 1] == 'O') queue.add(row * n + col + 1);
+    }
+    for (int i = 0; i < m; i ++) {
+      for (int j = 0; j < n; j ++) {
+        if (board[i][j] == 'O')  board[i][j] = 'X';
+        if (board[i][j] == '1')  board[i][j] = 'O';
+      }
+    }
+    return;
+	}
+	
+	// dfs: This solution causes java.lang.StackOverflowError, because for a large board, 
+	// too many method calls are pushed to the stack and causes the overflow.
+	public void solve(char[][] board) {
+    if (board == null || board.length == 0 || board[0].length == 0)    return;
+    
+    int m = board.length, n = board[0].length;
+    for (int i = 0; i < n; i ++) {
+      if (board[0][i] == 'O') dfs(board, 0, i);
+      if (board[m - 1][i] == 'O') dfs(board, m - 1, i);
+    }
+    for (int i = 0; i < m; i ++) {
+      if (board[i][0] == 'O')  dfs(board, i, 0);
+      if (board[i][n - 1] == 'O')  dfs(board, i, n - 1);
+    }
+    for (int i = 0; i < m; i ++) {
+      for (int j = 0; j < n; j ++) {
+        if (board[i][j] == 'O')  board[i][j] = 'X';
+        if (board[i][j] == '1')  board[i][j] = 'O';
+      }
+    }
+    return;
+  }
+  private void dfs(char[][] board, int i, int j) {
+    if (i < 0 || j < 0 || i >= board.length || j >= board[i].length) return;
+    if (board[i][j] != 'O') return;
+    board[i][j] = '1';
+    dfs(board, i - 1, j);
+    dfs(board, i + 1, j);
+    dfs(board, i, j - 1);
+    dfs(board, i, j + 1);
+  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -80,12 +136,13 @@ public class SurroundedRegions {
 		char[] c3 = {'X','O','X','O','X','O'};
 		char[] c4 = {'O','X','O','X','O','X'};
 		
-		char[][] c = {c1, c2, c3, c4};
+//		char[][] c = {c1, c2, c3, c4};
+		char[][] cc = {{'O','O','O'},{'O','O','O'},{'O','O','O'}};
 		
-		Solution(c);
+		solution2(cc);
 		
-		for (int i = 0; i < c.length; i ++)
-			System.out.println(Arrays.toString(c[i]));
+		for (int i = 0; i < cc.length; i ++)
+			System.out.println(Arrays.toString(cc[i]));
 	}
 
 }
